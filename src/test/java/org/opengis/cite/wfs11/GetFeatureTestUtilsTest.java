388a9d0e0b1c236a1632410ba27d56ac5ab37d75
Lyn Goltz <goltz@lat-lon.de>
2015-04-02 14:57:11 +0200

    fixed return type

--- src/test/java/org/opengis/cite/wfs11/GetFeatureTestUtilsTest.java
+++ src/test/java/org/opengis/cite/wfs11/GetFeatureTestUtilsTest.java
@@ -7,8 +7,10 @@ public class GetFeatureTestUtilsTest {
 	@Test
 	public void testTest() throws Exception {
 		String wfsCapabilities = "http://cite.lat-lon.de/deegree-webservices-3.3.6/services/wfs110?request=GetCapabilities&service=WFS";
-		FeatureData featureData = GetFeatureTestUtils.findFeatureTypeAndPropertyName(wfsCapabilities);
-		System.out.println(featureData);
+		String featureTypeAndPropertyName = GetFeatureTestUtils
+				.findFeatureTypeAndPropertyName(wfsCapabilities);
+
+		System.out.println(featureTypeAndPropertyName);
 	}
 
 }
