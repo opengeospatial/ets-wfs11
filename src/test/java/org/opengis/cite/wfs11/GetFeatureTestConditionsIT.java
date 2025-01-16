362c5366531a95d948ed49c7458388a4f8484653
Benjamin Proß <b.pross@52north.org>
2025-01-16 06:21:36 +0100

    Adjust test code

--- src/test/java/org/opengis/cite/wfs11/GetFeatureTestConditionsIT.java
+++ src/test/java/org/opengis/cite/wfs11/GetFeatureTestConditionsIT.java
@@ -15,45 +15,33 @@ import org.w3c.dom.Node;
 public class GetFeatureTestConditionsIT {
 
 	@Test
-	public void testCheckFeatureTypesDescribedInSchema_SingleFeatureType()
-			throws Exception {
+	public void testCheckFeatureTypesDescribedInSchema_SingleFeatureType() throws Exception {
 		boolean featureTypesDescribedInSchema = GetFeatureTestConditions
-				.checkFeatureTypesDescribedInSchema(
-						capabilitiesSingleFeatureType(),
-						describeFeatureTypeResponse());
+			.checkFeatureTypesDescribedInSchema(capabilitiesSingleFeatureType(), describeFeatureTypeResponse());
 
 		assertThat(featureTypesDescribedInSchema, is(true));
 	}
 
 	@Test
-	public void testCheckFeatureTypesDescribedInSchema_FeatureTypeFromImportedSchema()
-			throws Exception {
-		boolean featureTypesDescribedInSchema = GetFeatureTestConditions
-				.checkFeatureTypesDescribedInSchema(
-						capabilitiesFeatureTypeFromImportedSchema(),
-						describeFeatureTypeResponse());
+	public void testCheckFeatureTypesDescribedInSchema_FeatureTypeFromImportedSchema() throws Exception {
+		boolean featureTypesDescribedInSchema = GetFeatureTestConditions.checkFeatureTypesDescribedInSchema(
+				capabilitiesFeatureTypeFromImportedSchema(), describeFeatureTypeResponse());
 
 		assertThat(featureTypesDescribedInSchema, is(true));
 	}
 
 	@Test
-	public void testCheckFeatureTypesDescribedInSchema_UndefinedFeatureType()
-			throws Exception {
-		boolean featureTypesDescribedInSchema = GetFeatureTestConditions
-				.checkFeatureTypesDescribedInSchema(
-						capabilitiesSingleFeatureTypeUndefined(),
-						describeFeatureTypeResponse());
+	public void testCheckFeatureTypesDescribedInSchema_UndefinedFeatureType() throws Exception {
+		boolean featureTypesDescribedInSchema = GetFeatureTestConditions.checkFeatureTypesDescribedInSchema(
+				capabilitiesSingleFeatureTypeUndefined(), describeFeatureTypeResponse());
 
 		assertThat(featureTypesDescribedInSchema, is(false));
 	}
 
 	@Test
-	public void testCheckFeatureTypesDescribedInSchema_FeatureTypeWithWrongNamespace()
-			throws Exception {
-		boolean featureTypesDescribedInSchema = GetFeatureTestConditions
-				.checkFeatureTypesDescribedInSchema(
-						capabilitiesSingleFeatureTypeWrongNamespace(),
-						describeFeatureTypeResponse());
+	public void testCheckFeatureTypesDescribedInSchema_FeatureTypeWithWrongNamespace() throws Exception {
+		boolean featureTypesDescribedInSchema = GetFeatureTestConditions.checkFeatureTypesDescribedInSchema(
+				capabilitiesSingleFeatureTypeWrongNamespace(), describeFeatureTypeResponse());
 
 		assertThat(featureTypesDescribedInSchema, is(false));
 	}
@@ -82,12 +70,12 @@ public class GetFeatureTestConditionsIT {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		factory.setNamespaceAware(true);
 		DocumentBuilder builder = factory.newDocumentBuilder();
-		InputStream is = GetFeatureTestConditionsIT.class
-				.getResourceAsStream(resource);
+		InputStream is = GetFeatureTestConditionsIT.class.getResourceAsStream(resource);
 		try {
 			Document wfsCapabilities = builder.parse(is);
 			return wfsCapabilities.getDocumentElement();
-		} finally {
+		}
+		finally {
 			is.close();
 		}
 	}
